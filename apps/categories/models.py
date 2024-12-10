9349c157ebf333254a29ec87e4b3671cee6904ff
muhammadnur <muhammadnurpdp@gmail.com>
2024-12-10 20:38:34 +0500

    Commit

--- apps/categories/models.py
+++ apps/categories/models.py
@@ -1,15 +1,14 @@
 from django.db import models
 
-
 class Categories(models.Model):
-    name = models.CharField(max_length=100)
-    ru_name = models.CharField(max_length=100)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=100, verbose_name="Название (англ.)")
+    ru_name = models.CharField(max_length=100, verbose_name="Название (рус.)")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата обновления")
 
     class Meta:
-        verbose_name = "Category"
-        verbose_name_plural = "Categories"
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
 
     def __str__(self):
         return self.name
--- apps/telegram_users/models.py
+++ apps/telegram_users/models.py
@@ -6,20 +6,20 @@ class TelegramUsers(models.Model):
         ('ru', 'Ru'),
         ('uz', 'Uz'),
     ]
-    chat_id = models.CharField(max_length=255, unique=True)
-    username = models.CharField(max_length=255, null=True)
-    full_name = models.CharField(max_length=255)
-    phone_number = models.CharField(max_length=50)
-    latitude = models.FloatField(null=True)
-    longitude = models.FloatField(null=True)
-    location = models.CharField(max_length=255, null=True)
-    language = models.CharField(max_length=2, choices=LANGUAGE_CHOICES, default='uz')
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    chat_id = models.CharField(max_length=255, unique=True, verbose_name="Chat ID")
+    username = models.CharField(max_length=255, null=True, verbose_name="Username")
+    full_name = models.CharField(max_length=255, verbose_name="Full Name")
+    phone_number = models.CharField(max_length=50, verbose_name="Phone Number")
+    latitude = models.FloatField(null=True, verbose_name="Latitude")
+    longitude = models.FloatField(null=True, verbose_name="Longitude")
+    location = models.CharField(max_length=255, null=True, verbose_name="Location")
+    language = models.CharField(max_length=2, choices=LANGUAGE_CHOICES, default='uz', verbose_name="Language")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Date Created")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Date Updated")
 
     class Meta:
-        verbose_name = "Telegram User"
-        verbose_name_plural = "Telegram Users"
+        verbose_name = "Пользователь Telegram"
+        verbose_name_plural = "Пользователи Telegram"
 
     def __str__(self):
         return f"{self.full_name}"
