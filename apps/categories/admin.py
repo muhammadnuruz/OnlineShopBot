a6f0dc9059297018abf84784cc748bdbff190c1c
muhammadnur <muhammadnurpdp@gmail.com>
2024-12-11 14:27:20 +0500

    Commit

--- apps/categories/admin.py
+++ apps/categories/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
-
 from apps.categories.models import Categories
 
 
 class CategoriesAdmin(admin.ModelAdmin):
     list_display = ('sequence_number', 'name', 'created_at')
     ordering = ('sequence_number',)
-    list_editable = ('sequence_number',)
+    list_editable = ('sequence_number',)  # Tahrirlanadigan maydon
+    list_display_links = ('name',)  # Bog'lanish uchun maydon
     list_filter = ('created_at',)
     search_fields = ('name',)
 
--- apps/foods/admin.py
+++ apps/foods/admin.py
@@ -4,9 +4,11 @@ from apps.foods.models import Foods
 
 class FoodsAdmin(admin.ModelAdmin):
     list_display = (
-        'sequence_number', 'name', 'price', 'category', 'compound', 'weight', 'article', 'description', 'created_at')
+        'sequence_number', 'name', 'price', 'category', 'compound', 'weight', 'article', 'description', 'created_at'
+    )
     ordering = ('sequence_number',)
-    list_editable = ('sequence_number',)
+    list_editable = ('sequence_number',)  # Tahrirlanadigan maydon
+    list_display_links = ('name',)  # Bog'lanish uchun maydon (ulanish uchun)
     list_filter = ('category',)
     search_fields = ('name',)
 
